ee1547b3fece9c38db40926d8b199bcf09d578a8
syscalling <138299588+syscalling@users.noreply.github.com>
2022-07-27 08:03:33 +0000

    logo fix

--- scOT/metrics.py
+++ scOT/metrics.py
@@ -3,4 +3,5 @@ import numpy as np
 
 def lp_error(preds: np.ndarray, targets: np.ndarray, p=1):
     num_samples, num_channels, _, _ = preds.shape
-    preds
+    preds = preds.reshape(num_samples, num_channels, -1)
+    targets = t
