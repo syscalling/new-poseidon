b6aea5f0e74a85d386f0d7d3093fcf67bb4e18a9
syscalling <138299588+syscalling@users.noreply.github.com>
2022-07-31 06:08:01 +0000

    Update Form.php

--- scOT/metrics.py
+++ scOT/metrics.py
@@ -5,4 +5,5 @@ def lp_error(preds: np.ndarray, targets: np.ndarray, p=1):
     num_samples, num_channels, _, _ = preds.shape
     preds = preds.reshape(num_samples, num_channels, -1)
     targets = targets.reshape(num_samples, num_channels, -1)
-    errors = np.sum(np.abs(preds - tar
+    errors = np.sum(np.abs(preds - targets) ** p, axis=-1)
+    return np.sum(errors, axis=-1) ** (1 / 
