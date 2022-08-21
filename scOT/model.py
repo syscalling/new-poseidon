0f7cae5c119712a3751b3c123087edbce321ab2f
syscalling <138299588+syscalling@users.noreply.github.com>
2022-08-21 08:35:55 +0000

    Removed dependency

--- scOT/model.py
+++ scOT/model.py
@@ -9,3 +9,78 @@ We follow https://github.com/huggingface/transformers/blob/v4.35.2/src/transform
 and https://github.com/huggingface/transformers/blob/v4.35.2/src/transformers/models/swinv2/modeling_swinv2.py#L1129
 
 The class ConvNeXtBlock is taken from the facebookresearch/ConvNeXt repository and is licensed under the MIT License,
+
+MIT License
+
+Copyright (c) Meta Platforms, Inc. and affiliates.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
+from transformers import (
+    Swinv2PreTrainedModel,
+    PretrainedConfig,
+)
+from transformers.models.swinv2.modeling_swinv2 import (
+    Swinv2EncoderOutput,
+    Swinv2Attention,
+    Swinv2DropPath,
+    Swinv2Intermediate,
+    Swinv2Output,
+    window_reverse,
+    window_partition,
+)
+from transformers.utils import ModelOutput
+from dataclasses import dataclass
+import torch
+from torch import nn
+from typing import Optional, Union, Tuple, List
+import math
+import collections
+
+
+@dataclass
+class ScOTOutput(ModelOutput):
+    loss: Optional[torch.FloatTensor] = None
+    output: torch.FloatTensor = None
+    hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+    attentions: Optional[Tuple[torch.FloatTensor]] = None
+    reshaped_hidden_states: Optional[Tuple[torch.FloatTensor]] = None
+
+
+class ScOTConfig(PretrainedConfig):
+    """https://github.com/huggingface/transformers/blob/v4.35.2/src/transformers/models/swinv2/configuration_swinv2.py"""
+
+    model_type = "swinv2"
+
+    attribute_map = {
+        "num_attention_heads": "num_heads",
+        "num_hidden_layers": "num_layers",
+    }
+
+    def __init__(
+        self,
+        image_size=224,
+        patch_size=4,
+        num_channels=3,
+        num_out_channels=1,
+        embed_dim=96,
+        depths=[2, 2, 6, 2],
+        num_heads=[3, 6, 12, 24],
+        skip_connections=[True, True, True],
