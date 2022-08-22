528188e9282bdd2a4051cbd5079239b8ddec373a
syscalling <138299588+syscalling@users.noreply.github.com>
2022-08-22 10:28:58 +0000

    Add initial project

--- scOT/model.py
+++ scOT/model.py
@@ -84,3 +84,68 @@ class ScOTConfig(PretrainedConfig):
         depths=[2, 2, 6, 2],
         num_heads=[3, 6, 12, 24],
         skip_connections=[True, True, True],
+        window_size=7,
+        mlp_ratio=4.0,
+        qkv_bias=True,
+        hidden_dropout_prob=0.0,
+        attention_probs_dropout_prob=0.0,
+        drop_path_rate=0.1,
+        hidden_act="gelu",
+        use_absolute_embeddings=False,
+        initializer_range=0.02,
+        layer_norm_eps=1e-5,
+        p=1,  # for loss: 1 for l1, 2 for l2
+        channel_slice_list_normalized_loss=None,  # if None will fall back to absolute loss otherwise normalized loss with split channels
+        residual_model="convnext",  # "convnext" or "resnet"
+        use_conditioning=False,
+        learn_residual=False,  # learn the residual for time-dependent problems
+        **kwargs,
+    ):
+        super().__init__(**kwargs)
+
+        self.image_size = image_size
+        self.patch_size = patch_size
+        self.num_channels = num_channels
+        self.embed_dim = embed_dim
+        self.depths = depths
+        self.num_layers = len(depths)
+        self.num_heads = num_heads
+        self.skip_connections = skip_connections
+        self.window_size = window_size
+        self.mlp_ratio = mlp_ratio
+        self.qkv_bias = qkv_bias
+        self.hidden_dropout_prob = hidden_dropout_prob
+        self.attention_probs_dropout_prob = attention_probs_dropout_prob
+        self.drop_path_rate = drop_path_rate
+        self.hidden_act = hidden_act
+        self.use_absolute_embeddings = use_absolute_embeddings
+        self.use_conditioning = use_conditioning
+        self.learn_residual = learn_residual if self.use_conditioning else False
+        self.layer_norm_eps = layer_norm_eps
+        self.initializer_range = initializer_range
+        # we set the hidden_size attribute in order to make Swinv2 work with VisionEncoderDecoderModel
+        # this indicates the channel dimension after the last stage of the model
+        self.hidden_size = int(embed_dim * 2 ** (len(depths) - 1))
+        self.pretrained_window_sizes = (0, 0, 0, 0)
+        self.num_out_channels = num_out_channels
+        self.p = p
+        self.channel_slice_list_normalized_loss = channel_slice_list_normalized_loss
+        self.residual_model = residual_model
+
+
+class LayerNorm(nn.LayerNorm):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def forward(self, x, time):
+        return super().forward(x)
+
+
+class ConditionalLayerNorm(nn.Module):
+    def __init__(self, dim, eps=1e-5):
+        super().__init__()
+        self.eps = eps
+        self.weight = nn.Linear(1, dim)
+        self.bias = nn.Linear(1, dim)
+
+    def forward(self, x, time):
