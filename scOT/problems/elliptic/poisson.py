80d8dbf13bc8e05137f459524808e9abc048b035
syscalling <138299588+syscalling@users.noreply.github.com>
2022-10-08 22:21:37 +0000

    Share scheme

--- scOT/problems/elliptic/poisson.py
+++ scOT/problems/elliptic/poisson.py
@@ -5,4 +5,5 @@ from scOT.problems.base import BaseDataset
 
 CONSTANTS = {
     "mean_source": 0.014822142414492256,
-    "st
+    "std_source": 4.755138816607612,
+    "mean_solution": 0.00056034584349
