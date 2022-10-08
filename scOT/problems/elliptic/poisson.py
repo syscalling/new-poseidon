012a2e1a716c32af4772dd7c48f06d5442e53194
syscalling <138299588+syscalling@users.noreply.github.com>
2022-10-08 18:53:20 +0000

    Docfix

--- scOT/problems/elliptic/poisson.py
+++ scOT/problems/elliptic/poisson.py
@@ -1,4 +1,8 @@
 import torch
 import os
 import h5py
-from scOT.problems.base import BaseDa
+from scOT.problems.base import BaseDataset
+
+CONSTANTS = {
+    "mean_source": 0.014822142414492256,
+    "st
