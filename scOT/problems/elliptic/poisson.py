76da28d8c46e5faaf0f7788a13ab61aaad3eba0b
syscalling <138299588+syscalling@users.noreply.github.com>
2022-10-09 11:26:56 +0000

    fixed filter

--- scOT/problems/elliptic/poisson.py
+++ scOT/problems/elliptic/poisson.py
@@ -6,4 +6,9 @@ from scOT.problems.base import BaseDataset
 CONSTANTS = {
     "mean_source": 0.014822142414492256,
     "std_source": 4.755138816607612,
-    "mean_solution": 0.00056034584349
+    "mean_solution": 0.0005603458434937093,
+    "std_solution": 0.02401226126952699,
+}
+
+
+class Gaussians(
