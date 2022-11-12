a11fea45195927295a6a1abd2c2d25f3d9f481ca
syscalling <138299588+syscalling@users.noreply.github.com>
2022-11-12 20:46:29 +0000

    Changed font

--- scOT/problems/fluids/compressible.py
+++ scOT/problems/fluids/compressible.py
@@ -82,3 +82,71 @@ class RichtmyerMeshkov(BaseTimeDataset):
         self.label_description = "[rho],[u,v],[p]"
 
         self.pixel_mask = torch.tensor([False, False, False, False])
+
+        self.post_init()
+
+    def __getitem__(self, idx):
+        i, t, t1, t2 = self._idx_map(idx)
+        time = t / self.constants["time"]
+
+        inputs = (
+            torch.from_numpy(self.reader["solution"][i + self.start, t1, 0:4])
+            .type(torch.float32)
+            .reshape(4, self.resolution, self.resolution)
+        )
+
+        label = (
+            torch.from_numpy(self.reader["solution"][i + self.start, t2, 0:4])
+            .type(torch.float32)
+            .reshape(4, self.resolution, self.resolution)
+        )
+
+        inputs = (inputs - self.constants["mean"]) / self.constants["std"]
+        label = (label - self.constants["mean"]) / self.constants["std"]
+
+        return {
+            "pixel_values": inputs,
+            "labels": label,
+            "time": time,
+            "pixel_mask": self.pixel_mask,
+        }
+
+
+class RayleighTaylor(BaseTimeDataset):
+    def __init__(self, *args, tracer=False, **kwargs):
+        super().__init__(*args, **kwargs)
+        assert self.max_num_time_steps * self.time_step_size <= 10
+
+        self.N_max = 1260
+        self.N_val = 100
+        self.N_test = 130
+        self.resolution = 128
+
+        data_path = self.data_path + "/GCE-RT.nc"
+        data_path = self._move_to_local_scratch(data_path)
+        self.reader = h5py.File(data_path, "r")
+
+        self.constants = {
+            "mean": torch.tensor(
+                [0.8970493, 4.0316996e-13, -1.3858967e-13, 0.7133829, -1.7055787]
+            )
+            .unsqueeze(1)
+            .unsqueeze(1),
+            "std": torch.tensor(
+                [0.12857835, 0.014896976, 0.014896975, 0.21293919, 0.40131348]
+            )
+            .unsqueeze(1)
+            .unsqueeze(1),
+            "time": 10.0,
+        }
+
+        self.input_dim = 5
+        self.label_description = "[rho],[u,v],[p],[g]"
+
+        self.pixel_mask = torch.tensor([False, False, False, False, False])
+
+        self.post_init()
+
+    def __getitem__(self, idx):
+        i, t, t1, t2 = self._idx_map(idx)
+        time = t / self.constants["time"]
