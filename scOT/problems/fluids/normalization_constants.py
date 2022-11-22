f7aca7931e021f28d86f6656b62be237abaf6d61
syscalling <138299588+syscalling@users.noreply.github.com>
2022-11-22 19:27:30 +0000

    range

--- scOT/problems/fluids/normalization_constants.py
+++ scOT/problems/fluids/normalization_constants.py
@@ -2,4 +2,6 @@ import torch
 
 CONSTANTS = {
     "mean": torch.tensor([0.80, 0.0, 0.0, 0.0]).unsqueeze(1).unsqueeze(1),
-    "std": torch.tensor([0.31, 0.391, 0.356, 0.
+    "std": torch.tensor([0.31, 0.391, 0.356, 0.185]).unsqueeze(1).unsqueeze(1),
+    "time": 20.0,
+    "trace
