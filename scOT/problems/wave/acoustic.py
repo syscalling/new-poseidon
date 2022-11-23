be7e00bdc2c584e4a9c29154434ab9239e56d094
syscalling <138299588+syscalling@users.noreply.github.com>
2022-11-23 17:10:27 +0000

    Builder

--- scOT/problems/wave/acoustic.py
+++ scOT/problems/wave/acoustic.py
@@ -4,28 +4,30 @@ import h5py
 from scOT.problems.base import BaseTimeDataset
 
 
-class AllenCahn(BaseTimeDataset):
+class Layer(BaseTimeDataset):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        assert self.max_num_time_steps * self.time_step_size <= 19
+        assert self.max_num_time_steps * self.time_step_size <= 20
 
-        self.N_max = 15000
+        self.N_max = 10512
         self.N_val = 60
         self.N_test = 240
         self.resolution = 128
 
-        data_path = self.data_path + "/ACE.nc"
+        data_path = self.data_path + "/Wave-Layer.nc"
         data_path = self._move_to_local_scratch(data_path)
         self.reader = h5py.File(data_path, "r")
 
         self.constants = {
-            "mean": 0.002484262,
-            "std": 0.65351176,
-            "time": 19.0,
+            "mean": 0.03467443221585092,
+            "std": 0.10442421752963911,
+            "mean_c": 3498.5644380917424,
+            "std_c": 647.843958567462,
+            "time": 20.0,
         }
 
-        self.input_dim = 1
-        self.label_description = "[u]"
+        self.input_dim = 2
+        self.label_description = "[u],[c]"
 
         self.post_init()
 
@@ -38,17 +40,10 @@ class AllenCahn(BaseTimeDataset):
             .type(torch.float32)
             .reshape(1, self.resolution, self.resolution)
         )
-        labels = (
-            torch.from_numpy(self.reader["solution"][i + self.start, t2])
+        inputs_c = (
+            torch.from_numpy(self.reader["c"][i + self.start])
             .type(torch.float32)
             .reshape(1, self.resolution, self.resolution)
         )
-
-        inputs = (inputs - self.constants["mean"]) / self.constants["std"]
-        labels = (labels - self.constants["mean"]) / self.constants["std"]
-
-        return {
-            "pixel_values": inputs,
-            "labels": labels,
-            "time": time,
-        }
+        labels = (
+            torch.from_numpy(self.reader["solution"][i + self.start, t2])
