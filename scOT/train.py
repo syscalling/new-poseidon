c4b61670b2b298f6c4cf1040fc03633c21af5542
syscalling <138299588+syscalling@users.noreply.github.com>
2022-11-25 14:21:08 +0000

    Updated environment

--- scOT/train.py
+++ scOT/train.py
@@ -1,2 +1,4 @@
 """
-This script trains a scOT or pretrains Poseidon on a PDE da
+This script trains a scOT or pretrains Poseidon on a PDE dataset.
+Can be also used for finetuning Poseidon.
+Can be used in a
