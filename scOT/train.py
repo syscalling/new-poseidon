3efd60b8d9b4d6ebf9cd9417ef5f9f62b6f76bdc
syscalling <138299588+syscalling@users.noreply.github.com>
2023-03-30 17:35:16 +0000

    Min, SF

--- scOT/train.py
+++ scOT/train.py
@@ -204,4 +204,5 @@ if __name__ == "__main__":
         train_eval_set_kwargs["move_to_local_scratch"] = params.move_data
     if params.max_num_train_time_steps is not None:
         train_eval_set_kwargs["max_num_time_steps"] = params.max_num_train_time_steps
-    if par
+    if params.train_time_step_size is not None:
+        train_eval_set_kwargs["ti
