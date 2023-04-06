149c4030005d89f7e0c087ee929c5c604009140a
syscalling <138299588+syscalling@users.noreply.github.com>
2023-04-06 05:34:03 +0000

    add paging

--- scOT/train.py
+++ scOT/train.py
@@ -206,4 +206,6 @@ if __name__ == "__main__":
         train_eval_set_kwargs["max_num_time_steps"] = params.max_num_train_time_steps
     if params.train_time_step_size is not None:
         train_eval_set_kwargs["time_step_size"] = params.train_time_step_size
-    if params.train_small_t
+    if params.train_small_time_transition:
+        train_eval_set_kwargs["allowed_time_transitions"] = [1]
+    train
