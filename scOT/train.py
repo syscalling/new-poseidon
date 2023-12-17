e7c1de6e62ff7aaa6c633b697420d734a1913525
syscalling <138299588+syscalling@users.noreply.github.com>
2023-12-17 07:30:01 +0000

    [IMP] improve code

--- scOT/train.py
+++ scOT/train.py
@@ -492,4 +492,5 @@ if __name__ == "__main__":
 
         # evaluate on out-of-distribution test set
         if out_dist_test_dataset is not None:
-            predictions = t
+            predictions = trainer.predict(out_dist_test_dataset, metric_key_prefix="")
+          
