eee1bce0542a04ff3ab1cf40c284126b41b8fe9f
syscalling <138299588+syscalling@users.noreply.github.com>
2024-03-31 21:09:02 +0000

    A few more tweaks.

---
+++ README.md
@@ -0,0 +1,22 @@
+
+# New Poseidon: Enhanced Efficient Foundation Models for PDEs
+This is the enhanced version of the original source code for the paper [*Poseidon: Efficient Foundation Models for PDEs*](https://arxiv.org/abs/2405.19101). This repository also acts as a package if you want to use the enhanced models in your code.
+
+![Poseidon](assets/fig1.png)
+
+Use the pretrained models and pretraining dataset in our assortment on the [🤗 Hub – Pretrained Models and Pretraining Datasets](https://huggingface.co/collections/syscalling/new-poseidon-664fa125729c53d8607e209a). All datasets corresponding to downstream tasks can be downloaded from the same collection on the [🤗 Hub – Downstream Tasks](https://huggingface.co/collections/syscalling/new-poseidon-downstream-tasks-664fa237cd6b0c097971ef14) as well. To use them, follow the corresponding sections below.
+
+[...CONTENT OMITTED FOR BREVITY...]
+
+If you use our models, code, or datasets, please consider citing our paper, details below:
+
+```bibtex
+@misc{herde2024poseidon,
+      title={Poseidon: Efficient Foundation Models for PDEs}, 
+      author={Maximilian Herde and Bogdan Raonić and Tobias Rohner and Roger Käppeli and Roberto Molinaro and Emmanuel de Bézenac and Siddhartha Mishra},
+      year={2024},
+      eprint={2405.19101},
+      archivePrefix={arXiv},
+      primaryClass={cs.LG}
+}
+```
--- scOT/utils.py
+++ scOT/utils.py
@@ -46,3 +46,52 @@ def read_cli(parser):
         action="store_true",
         help="Whether to train only for next step prediction.",
     )
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        required=True,
+        help="Base path to data.",
+    )
+    parser.add_argument(
+        "--checkpoint_path",
+        type=str,
+        required=True,
+        help="Path to checkpoint directory. Will be prepended by wandb project and run name.",
+    )
+    parser.add_argument(
+        "--disable_tqdm",
+        action="store_true",
+        help="Whether to disable tqdm progress bar",
+    )
+    parser.add_argument(
+        "--push_to_hf_hub",
+        type=str,
+        default=None,
+        help="Whether to push the model to Huggingface Hub. Specify the model repository name.",
+    )
+    parser.add_argument(
+        "--just_velocities",
+        action="store_true",
+        help="Whether to only use velocities as input. Only relevant for incompressible flow datasets.",
+    )
+    parser.add_argument(
+        "--move_data",
+        str=None,
+        help="If set, moves the data to this directory and trains from there.",
+    )
+    return parser
+
+
+def get_num_parameters(model):
+    """Returns the number of trainable parameters in a model."""
+
+    return sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+
+def get_num_parameters_no_embed(model):
+    """Returns the number of trainable parameters in a scOT model without embedding and recovery."""
+    out = 0
+    for name, p in model.named_parameters():
+        if not ("embeddings" in name or "patch_recovery" in name) and p.requires_grad:
+            out += p.numel()
+    return out
